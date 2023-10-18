93148c58fe78b719fb5cc4b64790a9665bc17767
zaidiyazdan <117353242+zaidiyazdan@users.noreply.github.com>
2023-10-18 15:00:27 +0530

    completed the project with all the fileration process ,haveing numeric filter,sort,pagination

--- controllers/products.js
+++ controllers/products.js
@@ -5,13 +5,13 @@ const getAllProductsStatic = async (req,res) =>{
     // const search = 'oo';
     //here we are using regex to seacrch for pattern and i is used as a optoion which ignores case senstivity
     // name: {$regex: search,$options: 'i'}
-    const products = await Product.find({}).sort("-name price").limit(20);
+    const products = await Product.find({}).select("name price");
     res.status(200).json({ products , nbHits: products.length})
 }
 
 const getAllProducts = async (req,res) =>{
     console.log(req.query);
-    const {featured,company,name,sort} = req.query;
+    const {featured,company,name,sort,fields,numericFilters } = req.query;
     //we will make a queryObject where we will only those data which are present in the model.
     const queryObject = {}
     
@@ -26,16 +26,55 @@ const getAllProducts = async (req,res) =>{
     {
         queryObject.name = {$regex: name,$options: 'i'};
     }
-    if(sort)
-    {
-
+    if(numericFilters)
+    {   
+        const operatorMap = {
+            '>' : '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<' : '$lt',
+            '<=' : '$lte',
+        }
+        const regEx = /\b(<|>|>=|<=|=)\b/g 
+        let filters = numericFilters.replace(regEx, (match) => {
+            return `-${operatorMap[match]}-`; // Return the replaced value
+        });
+        const options = ['price','rating'];
+        filters = filters.split(',').forEach((item)=>{
+            const [field,operator,value] = item.split('-');
+            if(options.includes(field))
+            {
+                queryObject[field] = {[operator]:Number(value)}
+            }
+        })
+        console.log(numericFilters)
+        console.log(filters);
     }
     console.log(queryObject);
     
-    let products = await Product.find(queryObject).sort();
+    let result = Product.find(queryObject).sort();
     if(sort){
-        
+        console.log(sort);
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
     }
+    else{
+        result = result.sort('createAt');
+    }
+    //for selectinc specific fields only
+    if(fields)
+    {
+        const filedList = fields.split(',').join(' ');
+        result = result.select(filedList);
+    }
+    //setting up skip and limit to add pagination functionality.
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1)*limit;
+
+    result = result.skip(skip).limit(limit);
+    const products = await result;
+
     res.status(200).json({msg: products,nhHits: products.length})
 }
 
